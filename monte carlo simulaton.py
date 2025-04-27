0eab33a9b44a2b053a27da37a8ab5e268c502bd4
AbirAbidi <abira7344@gmail.com>
2025-04-27 19:15:08 +0100

    the done deal

--- monte carlo simulaton.py
+++ monte carlo simulaton.py
@@ -14,8 +14,37 @@ nbr_reussite = 0
 
 # Uniform(a,b) return a random number between a and b
 # the * inside the uniform just unpack the tuple so instad of of range_concentration_en_classe[0] et range_concentration_en_classe[1]
-for simulation in (nbr_simulation):
+for simulation in range(nbr_simulation):
+    moyenne = 0
     
-    concentration = random.uniform(*range_concentration_en_classe)
-    clarte = random.uniform(*taux_clarte_prof)
-    comprendre_seule = random.uniform(*range_comprendre_seule_maison)
+    for cour in range(courses):
+        knowledge_gained_course = 0
+        
+        ma_presence = random.uniform(0, 1)
+        concentration = random.uniform(*range_concentration_en_classe)
+        clarte = random.uniform(*taux_clarte_prof)
+        comprendre_seule = random.uniform(*range_comprendre_seule_maison)
+        
+        if ma_presence <= taux_presence:
+            knowledge_gained_course += 3  
+            
+        if concentration >= 0.5  :
+            knowledge_gained_course += 5  
+            
+        if clarte >= 0.5:
+            knowledge_gained_course += 5  
+        
+        if comprendre_seule >= 0.5:
+            knowledge_gained_course += 7 
+        
+        moyenne += knowledge_gained_course
+    
+    moyenne = moyenne /courses
+          
+
+    if moyenne >= reussite:
+        nbr_reussite += 1 
+
+taux_reussite = nbr_reussite / nbr_simulation
+print(f"Taux de réussite estimé : {taux_reussite * 100:.2f}%")
+        
